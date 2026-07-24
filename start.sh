fd559636779847f6d694d773fd68d11aa2cfd12c
George Moseti <mosetigeorge@gmail.com>
2026-07-24 23:52:13 +0000

    Trigger CI with auto-deploy

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ if [ ! -f /app/db.sqlite3 ] || [ ! -s /app/db.sqlite3 ]; then
     echo "Seeding database..."
     python /app/seed.py
 fi
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000# auto-deploy test
